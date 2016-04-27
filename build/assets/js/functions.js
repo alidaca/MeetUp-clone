2bc583a84c2b34927bbd729c6a7b584cb07b6a83
Laura Holm <alidaca@msn.com>
2016-04-27 13:09:55 -0700

    created login modal

--- build/assets/js/functions.js
+++ build/assets/js/functions.js
@@ -1,5 +1,28 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
   // Get started!
 
 });
+
+
+//functions for modal login
+$('#modal-btn-login').on('click', function(){
+  $('.main-login').hide();
+  $('.email-login').show();
+  $('.modal-title').text('Login with your email');
+});
+
+$('#modal-btn-signUp').on('click', function(){
+  $('.main-login').hide();
+  $('.signup-form').show();
+  $('.modal-title').text('Register');
+});
+
+$('.modal-btn-back').on('click', function(){
+  $('.email-login').hide();
+  $('.signup-form').hide();
+  $('.main-login').show();
+  $('.modal-title').text('Login to create an event');
+});
+
+$('.modal-trigger').leanModal({overlay: 0.6, closeButton: '.modal-close'});
