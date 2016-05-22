92a9dab8f75a85a7d40cf95c366b3fee76d06181
Laura Holm <alidaca@msn.com>
2016-05-22 11:14:46 -0700

    fixed bug: new event form now opens when clicking Add Event after user logs in

---
+++ build/assets/js/app/addEventForm.js
@@ -0,0 +1,23 @@
+define(['jquery', 'app/googleApi'],function($, googleApi){
+
+  var addEventForm = {
+
+    init: function(){
+      this.cacheDom();
+    },
+
+    cacheDom: function(){
+      this.$eventForm = $('#eventForm');
+      this.$eventType = this.$eventForm.find('#eventType');
+      this.$eventDate = this.$eventForm.find('#eventDate');
+      this.$eventStart = this.$eventForm.find('#eventStart');
+      this.$eventEnd = this.$eventForm.find('#eventEnd');
+      this.$eventLocation = this.$eventForm.find('#eventLocation');
+      this.$eventHost = this.$eventForm.find('#hostName');
+      this.$eventDetails = this.$eventForm.find('#eventDetails');
+      this.$continueBtn = this.$eventForm.find('#continueBtn');
+    }
+  };
+
+  return addEventForm;
+});
