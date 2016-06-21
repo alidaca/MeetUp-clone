7aedc47b215e5c8fca6350eef1673f1ed59ac3b7
Laura Holm <alidaca@msn.com>
2016-06-20 22:12:44 -0700

    setting up email login

--- assets/js/app/home.js
+++ assets/js/app/home.js
@@ -8,7 +8,11 @@ requirejs(['jquery', 'leanModal'],function($, leanModal){
     modalReg.init();
   });
 
-  requirejs(['https://alidaca.github.io/MeetUp-clone/assets/js/app/modal-email.js'], function(modalEmail){
+  // requirejs(['https://alidaca.github.io/MeetUp-clone/assets/js/app/modal-email.js'], function(modalEmail){
+  //   modalEmail.init();
+  // });
+
+  requirejs(['app/modal-email.js'], function(modalEmail){
     modalEmail.init();
   });
 
