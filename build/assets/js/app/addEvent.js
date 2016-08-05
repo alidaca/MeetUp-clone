0281744afc901248d9fcf52023ab0dd3c6ababf5
Laura Holm <alidaca@msn.com>
2016-08-04 21:51:27 -0700

    changed links to work with gh-pages

--- build/assets/js/app/addEvent.js
+++ build/assets/js/app/addEvent.js
@@ -1,43 +1,43 @@
 requirejs(['jquery', 'leanModal'],function($, leanModal){
 
-  requirejs(['/assets/js/app/modal-main.js'],function(modalMain){
-    modalMain.init();
-  });
-
-  requirejs(['/assets/js/app/modal-reg.js'], function(modalReg){
-    modalReg.init();
-  });
-
-  requirejs(['/assets/js/app/firebase-auth.js'], function(firebaseAuth){
-    firebaseAuth.init();
-  });
-
-  require(['googlemaps','/assets/js/app/addEventForm.js'],function(GoogleMapsLoader,addEventForm){
-    GoogleMapsLoader.done(function(){
-      addEventForm.init();
-    }).fail(function(){
-      console.error("ERROR: Google maps library failed to load");
-    });
-  });
-
-  // requirejs(['https://alidaca.github.io/MeetUp-clone/assets/js/app/modal-main.js'],function(modalMain){
+  // requirejs(['/assets/js/app/modal-main.js'],function(modalMain){
   //   modalMain.init();
   // });
 
-  // requirejs(['https://alidaca.github.io/MeetUp-clone/assets/js/app/modal-reg.js'], function(modalReg){
+  // requirejs(['/assets/js/app/modal-reg.js'], function(modalReg){
   //   modalReg.init();
   // });
 
-  // requirejs(['https://alidaca.github.io/MeetUp-clone/assets/js/app/firebase-auth.js'], function(firebaseAuth){
+  // requirejs(['/assets/js/app/firebase-auth.js'], function(firebaseAuth){
   //   firebaseAuth.init();
   // });
 
-  // require(['googlemaps','https://alidaca.github.io/MeetUp-clone/assets/js/app/addEventForm.js'],function(GoogleMapsLoader,addEventForm){
+  // require(['googlemaps','/assets/js/app/addEventForm.js'],function(GoogleMapsLoader,addEventForm){
   //   GoogleMapsLoader.done(function(){
   //     addEventForm.init();
   //   }).fail(function(){
   //     console.error("ERROR: Google maps library failed to load");
   //   });
   // });
+
+  requirejs(['https://alidaca.github.io/MeetUp-clone/assets/js/app/modal-main.js'],function(modalMain){
+    modalMain.init();
+  });
+
+  requirejs(['https://alidaca.github.io/MeetUp-clone/assets/js/app/modal-reg.js'], function(modalReg){
+    modalReg.init();
+  });
+
+  requirejs(['https://alidaca.github.io/MeetUp-clone/assets/js/app/firebase-auth.js'], function(firebaseAuth){
+    firebaseAuth.init();
+  });
+
+  require(['googlemaps','https://alidaca.github.io/MeetUp-clone/assets/js/app/addEventForm.js'],function(GoogleMapsLoader,addEventForm){
+    GoogleMapsLoader.done(function(){
+      addEventForm.init();
+    }).fail(function(){
+      console.error("ERROR: Google maps library failed to load");
+    });
+  });
 
 });
--- source/assets/js/addGuests-main.js
+++ source/assets/js/addGuests-main.js
@@ -1,5 +1,5 @@
 requirejs(['./common'], function (common) {
-      requirejs(['/assets/js/app/addGuests.js']);
+      // requirejs(['/assets/js/app/addGuests.js']);
 
-    // requirejs(['https://alidaca.github.io/MeetUp-clone/assets/js/app/addGuests.js']);
+    requirejs(['https://alidaca.github.io/MeetUp-clone/assets/js/app/addGuests.js']);
 });
